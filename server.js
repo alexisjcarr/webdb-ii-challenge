2d784b06f4be3b0812b522a840df8a623befabbd
alexisjcarr <acarr59@gmail.com>
2019-07-16 13:27:54 -0500

    🎉 Adds express app boilerplate.
     #1

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// const db = require("./data/dbConfig");
+
+const server = express();
+
+server.use(bodyParser.json());
+
+// routers
+
+module.exports = server;
